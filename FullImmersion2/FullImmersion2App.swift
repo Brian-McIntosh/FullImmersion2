394a7fd26be1834b6f1d1f754bb1fd1480b29d6f
Brian McIntosh <brian@mixinteractive.com>
2023-12-25 09:46:16 -0500

    Initial Commit

---
+++ FullImmersion2/FullImmersion2App.swift
@@ -0,0 +1,17 @@
+//
+//  FullImmersion2App.swift
+//  FullImmersion2
+//
+//  Created by Brian McIntosh on 12/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FullImmersion2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
